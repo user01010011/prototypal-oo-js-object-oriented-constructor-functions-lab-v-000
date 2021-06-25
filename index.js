4ec9d5365e984bbc30776fe38aed1bcff22e7db5
Shasha Yan <shashayan@gmail.com>
2021-06-25 05:40:33 -0500

    commit tree bff581153f8f3cc1e8627fbfc3158c2907434fd8

---
+++ index.js
@@ -0,0 +1,39 @@
+// class Scooter {
+//     constructer(year, color, model){
+//         this.year =  year;
+//         this.color = color;
+//         this.model = model;
+//     }
+// }
+
+// class Driver {
+//     constructer(name, age, experience){
+//         this.name = name;
+//         this.age = age;
+//         this.experience = experience;
+//     }
+// }
+
+// class PickupLocation {
+//     constructer(address, city) {
+//         this.address = address;
+//         this.city = city;
+//     }
+// }
+
+function Scooter(year, color, model){
+    this.year = year;
+    this.color = color;
+    this.model = model;
+}
+
+function Driver(name, age, experience){
+    this.name = name;
+    this.age = age;
+    this.experience = experience;
+}
+
+function PickupLocation(address, city){
+    this.address = address;
+    this.city = city;
+}
